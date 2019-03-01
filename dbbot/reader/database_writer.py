9acc129f9a3f9591f10aea8ddf57554397609a22
unixo <unixo@devzero.it>
2019-03-01 19:20:35 +0100

    restored lines wrongly deleted

--- dbbot/reader/database_writer.py
+++ dbbot/reader/database_writer.py
@@ -178,6 +178,8 @@ class DatabaseWriter(RobotDatabase):
         self._connection.execute(sql_statement)
 
     def fetch_id(self, table_name, criteria):
+        sql_statement = 'SELECT id FROM %s WHERE ' % table_name
+        sql_statement += ' AND '.join('%s=?' % key for key in criteria.keys())
         res = self._connection.execute(sql_statement, criteria.values()).fetchone()
         if not res:
             raise Exception('Query did not yield id, even though it should have.'
